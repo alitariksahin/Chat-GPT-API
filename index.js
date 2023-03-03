85f8a52c0479d6286793dd2a303132b4adc1a153
alitariksahin <alitariksah@gmail.com>
2023-03-03 14:42:35 +0300

    default properties set

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const start = async () => {
     try {
         const email = "example@gmail.com"; // type your email with which you registered chatGPT.
         const password = "123456"; // type your password with which you registered chatGPT.
-        //await login(email , password); // Once you login you can comment this line out.
+        await login(email , password); // Once you login you can comment this line out.
         
         const input = "write me an acrostic poem with the letters of chatgpt"; //here your input goes.
         const output = await chat(input);
